0b683dfd2f7cf41fa076cd6fa990700e5a7e61f8
Kochetov <kochetov.yevhen.dev@gmail.com>
2024-02-03 18:09:14 +0200

    create pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import RootLayout from "./RootLayout";
+
+const clientRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "forgot-password", element: <ForgotPassword /> },
+      { path: "offers", element: <Offers /> },
+      { path: "profile", element: <Profile /> },
+      { path: "signin", element: <SignIn /> },
+      { path: "signup", element: <SignUp /> },
+    ],
+  },
+]);
+
 function App() {
-  return <>Hello world</>;
+  return <RouterProvider router={clientRouter} />;
 }
 
 export default App;
