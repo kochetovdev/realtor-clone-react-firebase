cbdd3ead85439ff3213453b2506b9440c7e2a988
Kochetov <kochetov.yevhen.dev@gmail.com>
2024-02-03 18:42:36 +0200

    create header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,49 @@
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+import { Link, useLocation } from "react-router-dom";
 
-export default Header
+const Header = () => {
+  const { pathname } = useLocation();
+  const pathMatchRoute = (route: string) => route === pathname;
+
+  return (
+    <article className="bg-white border-b shadow-md sticky top-0 z-50">
+      <header className="flex justify-between items-center p-3 max-w-7xl mx-auto">
+        <div>
+          <Link to="/">
+            <img
+              src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+              alt="logo"
+              className="h-5 cursor-pointer"
+            />
+          </Link>
+        </div>
+        <div>
+          <ul className="flex space-x-10">
+            <li
+              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent cursor-pointer transition-colors ${
+                pathMatchRoute("/") && "text-black border-b-red-500"
+              }`}
+            >
+              <Link to="/">Home</Link>
+            </li>
+            <li
+              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent cursor-pointer transition-colors ${
+                pathMatchRoute("/offers") && "text-black border-b-red-500"
+              }`}
+            >
+              <Link to="/offers">Offers</Link>
+            </li>
+            <li
+              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent cursor-pointer transition-colors ${
+                pathMatchRoute("/signin") && "text-black border-b-red-500"
+              }`}
+            >
+              <Link to="/signin">Sign In</Link>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </article>
+  );
+};
+
+export default Header;
